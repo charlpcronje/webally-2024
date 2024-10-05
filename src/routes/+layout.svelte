a3d410a39af3d66ee56fa20153b37090634de926
Charl Cronje <charlcp@gmail.com>
2024-10-05 17:21:03 +0200

    Loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,46 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
+	import Analytics from '$lib/analytics.svelte';
 	import 'uno.css';
 	import NavMenu from '$lib/components/NavMenu/NavMenu.svelte';
 	import Footer from '$lib/components/Chip/Footer/Footer.svelte';
 	import '$lib/index.scss';
 	import { onHydrated } from '$lib/stores/theme';
 	import { onMount } from 'svelte';
+	import { loading } from '$lib/stores/loadingStore';
+	import PageTransition from '$lib/components/PageTransition.svelte';
 	import InstallPrompt from '$lib/components/InstallPrompt.svelte';
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+	
+	
+	// Function to show the loader
+	function showLoader() {
+		loading.set(true);
+	}
+
+	// Function to hide the loader
+	function hideLoader() {
+		setTimeout(() => loading.set(false), 500); // Add a slight delay to enhance UX
+	}
+
+	// Handle navigation events
+	beforeNavigate(() => {
+		showLoader();
+	});
+
+	afterNavigate(() => {
+		hideLoader();
+	});
+	
 	onMount(() => {
 		onHydrated();
+		setTimeout(() => {
+			hideLoader();
+		}, 1000);
 	});
 </script>
+<Analytics />
+<PageTransition />
 
 <div class="body contents theme-dark flex flex-col min-h-screen">
 	<NavMenu />
